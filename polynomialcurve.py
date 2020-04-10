Fixing space-unit conversion.

polynomialcurve.py
@@ -22,7 +22,7 @@ class PolynomialPointCurve:
         return ((data * 2 * self.space_size) - self.space_size)
 
     def compute_values(self, points): #Computes the polynomial on an input array of tuples. Input array should have dimensions (n_points, self.dimensions)
-        points = unit_to_space(points)
+        points = self.unit_to_space(points)
         out_vals = np.zeros(points.shape[0])
         for i in range (self.num_terms):
             pow_row = self.powers[i]
@@ -41,7 +41,7 @@ class PolynomialPointCurve:
         return self.threshold
 
     def compute_error(self, points, mode='mse'):  #Computes the error from 0 of the input points
-        points = unit_to_space(points)
+        points = self.unit_to_space(points)
         vals = self.compute_values(points)
         num_vals = points.shape[0]
         if mode == 'mse':
@@ -62,7 +62,7 @@ class PolynomialPointCurve:
             prosp_vals = self.compute_values(prosp_points)
             new_pts = prosp_points[prosp_vals<error_threshold]
             good_pts = np.concatenate([good_pts, new_pts], axis=0)
-        return space_to_unit(good_pts[:num_points])
+        return self.space_to_unit(good_pts[:num_points])
 
 
 